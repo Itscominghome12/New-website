e3dd223108d23c74e91b7ed2888ca6c03be0616b
Itscominghome12 <Cp92@mahurangi.school.nz>
2021-08-09 09:08:48 +1200

    About page version 1
    New features in about page

--- newabout.php
+++ newabout.php
@@ -9,9 +9,42 @@
 </div>
   
 <div class="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
+  <h1>About Us</h1>
+  <p>Canopy Cancer Care is a leading private clinic for adult cancer patients across New zealand. Our team specialise in the care of patients requiring cancer treatment with chemotherapy, immunotherapy, antibody therapy, hormone therapy and more targeted therapies.</p>
 </div>
+
+
+<div class="row">
+    <div class="col-sm-8">
+    
+    
+    <h2>Why choose us?</h2>
+        
+        
+    <p>
+We currently have 4 fully functioning clinics located in Auckland, North shore, Tauranga and Whangarei. With a new clinic being built in Wellington. We have over 350 staff that have integrated into the canopy team and thrive in the latest technology that Canopy offers its patients. Each clinic deals with at least 30 patients a day that can range from chemotherapy treatment to help and advice. 
+</p>
+        
+<img class="whyus" src="Images/seat.png">
+    
+    
+    
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    <div class="col-sm-4" >.col-sm-3
+    
+    
+    
+    
+    
+    </div>
+
 
 
 <?php include("Newfooter.html");?>
