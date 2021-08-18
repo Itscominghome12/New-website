83316591c06b2e7129600bcd18db8b0db1d93845
Itscominghome12 <Cp92@mahurangi.school.nz>
2021-08-18 18:02:20 +1200

    Version 6
    First phase testing complete about page. Changes to contact and head nav

--- newcontact.php
+++ newcontact.php
@@ -14,7 +14,7 @@
   
   </div>
   <div class="row">
-    <div class="column">
+    <div class="columnt">
       <img src="Images/freindly.png" style="width:75%">
         <img src="Images/seat.png" style="width:75%">
     </div>
@@ -24,11 +24,24 @@
         <input type="text" id="fname" name="firstname" placeholder="Your name..">
         <label for="lname">Last Name</label>
         <input type="text" id="lname" name="lastname" placeholder="Your last name..">
-        <label for="country">Country</label>
+        <label for="country">Region</label>
         <select id="country" name="country">
-          <option value="australia">North Island</option>
-          <option value="canada">South Island</option>
-          <option value="usa">Other</option>
+          <option value="australia">Northland</option>
+          <option value="canada">Auckland</option>
+        <option value="canada">Waikato</option>
+            <option value="canada">Bay of Plenty</option>
+            <option value="canada">Gisbourne</option>
+            <option value="canada">Hawkes Bay</option>
+            <option value="canada">Wellington</option>
+            <option value="canada">West coast</option>
+            <option value="canada">Southland</option>
+            <option value="canada">Taranaki</option>
+          <option value="usa">Tasman</option>
+            <option value="canada">Manawatu-Whanganui</option>
+            <option value="canada">Nelson</option>
+            <option value="canada">Marlborough</option>
+            <option value="canada">Cantebury</option>
+            <option value="canada">Otago</option>
         </select>
         <label for="subject">Subject</label>
         <textarea id="subject" name="subject" placeholder="Write something.." style="height:170px"></textarea>
