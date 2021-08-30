97d11e15872f4bca42c3bfc3d50d1f649dbf5ec9
Itscominghome12 <Cp92@mahurangi.school.nz>
2021-08-30 13:02:07 +1200

    Version 8
    Smaller chnaged to fonts and display throughout the website

--- newabout.php
+++ newabout.php
@@ -10,20 +10,20 @@
   
 <div class="about-section">
   <h1>About Canopy</h1>
-  <p>Canopy Cancer Care is a leading private clinic for adult cancer patients across New Zealand. Our team specialise in the care of patients requiring cancer treatment with chemotherapy, immunotherapy, antibody therapy, hormone therapy and more targeted therapies.
-</p>
+  <h4>Canopy Cancer Care is a leading private clinic for adult cancer patients across New Zealand. Our team specialise in the care of patients requiring cancer treatment with chemotherapy, immunotherapy, antibody therapy, hormone therapy and more targeted therapies.
+</h4>
 </div>
 
-<h2 style="text-align:center">Our Team</h2>
+<h1 style="text-align:center"><u>Our Team</u></h1>
 <div class="row">
 <div class="column">
 <div class="card">
 <img src="Images/istockphoto-1261229865-170667a.png" alt="Jane" style="width:100%">
       <div class="container">
         <h2>Jane Doe</h2>
-        <p class="title">CEO and Founder</p>
-        <p>Jane Doe founded Canopy Cancer Care in 2007, after 15 years experience as a nurse working in Auckland Jane saw an opportunity to open Canopy with help from her husband Greg. After 14 years Jane is still the current CEO at Canopy and helps to maintain the wonderful company she established. </p>
-        <p>janedoe@gmail.com</p>
+        <h4 class="title">CEO and Founder</h4>
+        <h5>Jane Doe founded Canopy Cancer Care in 2007, after 15 years experience as a nurse working in Auckland Jane saw an opportunity to open Canopy with help from her husband Greg. After 14 years Jane is still the current CEO at Canopy and helps to maintain the wonderful company she established. </h5>
+        <h5>janedoe@gmail.com</h5>
         <p><button class="button">Contact</button></p>
       </div>
     </div>
@@ -34,9 +34,9 @@
       <img src="Images/istockphoto-1270067126-170667a.png" alt="Mike" style="width:100%">
       <div class="container">
         <h2>Mike Ross</h2>
-        <p class="title">Team Management</p>
-        <p>Mike Ross has been a member of the company for 4 years now. After starting as an on-floor nurse, Mike has established himself as team manager for the main Epsom clinic in Auckland and helps maintain order, structure, and operation. Mike does this job because he wants to help a wonderful business do a great job.  </p>
-        <p>mross@hotmail.com</p>
+        <h4 class="title">Team Management</h4>
+        <h5>Mike Ross has been a member of the company for 4 years now. After starting as an on-floor nurse, Mike has established himself as team manager for the main Epsom clinic in Auckland and helps maintain order, structure, and operation. Mike does this job because he wants to help a wonderful business do a great job.  </h5>
+        <h5>mross@hotmail.com</h5>
         <p><button class="button">Contact</button></p>
       </div>
     </div>
@@ -47,9 +47,9 @@
       <img src="Images/istockphoto-1272687005-170667a.png" alt="John" style="width:100%">
       <div class="container">
         <h2>Eve Doe</h2>
-        <p class="title">Doctor</p>
-        <p>Eve Doe is the daughter of Jane and has recently started at canopy at the beginning of this year. After getting her degree from Auckland University she is now a key member of the Canopy team and is gaining experience with every passing day. Jane loves to help people and takes pride in her job at Canopy.</p>
-        <p>evedoe@gmail.com</p>
+        <h4 class="title">Doctor</h4>
+        <h5>Eve Doe is the daughter of Jane and has recently started at canopy at the beginning of this year. After getting her degree from Auckland University she is now a key member of the Canopy team and is gaining experience with every passing day. Jane loves to help people and takes pride in her job at Canopy.</h5>
+        <h5>evedoe@gmail.com</h5>
         <p><button class="button">Contact</button></p>
       </div>
     </div>
@@ -64,9 +64,9 @@
      
        <img src="Images/Whyus.png" alt="John" style="width:75%"> 
           
-          <h3>Why us</h3>
+        <h2 class="bold">Why us</h2>
           
-          <p>The Canopy team has been providing world-class services since 2010. We have built a strong team that are all passionate about our patients and the outcome that matters to them most. We aim to provide the highest quality of care and will coordinate all aspects of treatment where possible</p>
+          <h4 class="header4">The Canopy team has been providing world-class services since 2010. We have built a strong team that are all passionate about our patients and the outcome that matters to them most. We aim to provide the highest quality of care and will coordinate all aspects of treatment where possible</h4>
           
         
      
@@ -79,15 +79,15 @@
      
        <img src="Images/Locations.png" alt="John" style="width:75%">
          
-         <h3>Locations</h3>
+        <h2 class="bold">Locations</h2>
           
-          <p>We currently have 4 fully functioning clinics located in Auckland, North Shore, Tauranga and Whangarei. With a new clinic being built in Wellington. We have over 350 staff that have integrated into the canopy team and thrive in the latest technology that Canopy offers its patients. Each clinic deals with at least 30 patients a day, ranging from chemotherapy treatment to help and advice. </p>
+         <h4 class="header4">We currently have 4 fully functioning clinics located in Auckland, North Shore, Tauranga and Whangarei. With a new clinic being built in Wellington. We have over 350 staff that have integrated into the canopy team and thrive in the latest technology that Canopy offers its patients. Each clinic deals with at least 30 patients a day, ranging from chemotherapy treatment to help and advice. </h4>
      
      </div>
     </div>
 
 
-<?php include("Carousel.html");?>
+<?php include("Carousel copy.html");?>
   
 <?php include("Newfooter.html");?>
 
--- newcontact.php
+++ newcontact.php
@@ -8,18 +8,18 @@
  
 </div>
   
-<div class="container">
+<div class="containerw">
   <div style="text-align:center">
-    <h2>Contact Us</h2>
+    <h1><u>Contact Us</u></h1>
   
   </div>
   <div class="row">
     <div class="columnt">
-      <img src="Images/freindly.png" style="width:75%">
-        <img src="Images/seat.png" style="width:75%">
+      <img class="contactimg" src="Images/freindly.png">
+        <img class="contactimg" src="Images/seat.png">
     </div>
     <div class="column">
-      <form action="/action_page.php">
+      <form action="malito:connorpassingham@gmail.com" method="post" enctype="text/plain">
         <label for="fname">First Name</label>
         <input type="text" id="fname" name="firstname" placeholder="Your name..">
         <label for="lname">Last Name</label>
@@ -49,6 +49,48 @@
       </form>
     </div>
   </div>
+</div>
+
+<div class="containerl">
+    <div style="text-align:center">
+  <h2>Contact Numbers</h2>
+  <p>Call the numbers below to confirm or book an appointment</p>            
+  <table class="table table-hover">
+    <thead>
+      <tr>
+        <th>Location</th>
+        <th>Adress</th>
+        <th>Number</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Tauranga</td>
+        <td><a href="https://www.google.co.nz/maps/place/850+Cameron+Road,+Tauranga+South,+Tauranga+3112/@-37.7086974,176.1469533,17z/data=!3m1!4b1!4m5!3m4!1s0x6d6dda4c1cd2541f:0x5d2888a50482b2ba!8m2!3d-37.7087017!4d176.149142"><p style="color:blue ">850 Cameron Road, Tauranga South, Tauranga 3112</p></a></td>
+          <td><a href="url"><p style="color:blue ">02435682</p></a></td>
+      </tr>
+        <tr>
+        <td>North Shore</td>
+        <td><a href="https://www.google.co.nz/maps/place/213+Shakespeare+Road,+Milford,+Auckland+0620/@-36.7814374,174.751361,17z/data=!3m1!4b1!4m5!3m4!1s0x6d0d39c76c50fadd:0x92e9e9adaf6674d8!8m2!3d-36.7814417!4d174.7535497"><p style="color:blue ">213 Shakespeare Road, Takapuna, Auckland 0620</p></a></td>
+        <td><a href="url"><p style="color:blue ">02535772</p></a></td>
+      </tr>
+        <tr>
+        <td>Epsom (Auckland)</td>
+        <td><a href="https://www.google.co.nz/maps/place/98+Mountain+Road,+Epsom,+Auckland+1023/@-36.873416,174.7675692,17z/data=!3m1!4b1!4m5!3m4!1s0x6d0d47d711ba0461:0xcedafeef3ec31698!8m2!3d-36.8734203!4d174.7697579"><p style="color:blue ">98 Mountain Road, Epsom, Auckland 1023</p></a></td>
+        <td><a href="url"> <p style="color:blue ">03990127</p></a></td>
+      </tr>
+      <tr>
+        <td>Wellington</td>
+        <td><a href="https://www.google.co.nz/maps/place/234+Cuba+Street,+Te+Aro,+Wellington+6011/@-41.295905,174.7718029,17z/data=!3m1!4b1!4m5!3m4!1s0x6d38afd8855cc22b:0x41ac13a7a03ef00f!8m2!3d-41.2959091!4d174.7739916"><p style="color:blue ">234 Cuba Street, Te Aro, Wellington 6011</p></a></td>
+        <td><a href="url"><p style="color:blue ">02446219</p></a></td>
+      </tr>
+      <tr>
+        <td>Whangarai</td>
+        <td><a href="https://www.google.co.nz/maps/place/121+Bank+Street,+Kensington,+Whang%C4%81rei+0110/@-35.7212869,174.3180044,17z/data=!3m1!4b1!4m5!3m4!1s0x6d0b7ee67ef99cdf:0xc05aafd18c034746!8m2!3d-35.7212913!4d174.3201931"><p style="color:blue ">121 Bank Street, Kensington, Whangārei 0110</p></a></td>
+        <td><a href="url"><p style="color:blue ">02259134</p></a></td>
+      </tr>
+    </tbody>
+  </table>
 </div>
 
 <?php include("Newfooter.html");?>
